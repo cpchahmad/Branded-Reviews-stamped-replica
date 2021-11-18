864ab6311ec13240f535a61d066c1e2165fbe729
salman432 <salman.ali@tetralogicx.com>
2021-11-18 18:36:20 +0500

    commit

--- resources/views/append/popups.blade.php
+++ resources/views/append/popups.blade.php
@@ -115,3 +115,23 @@ $counter = 0;
     </div>
 </a>
 @endif
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $( ".owl-prev").html('<i class="fas fa-chevron-left"></i>');
+    $( ".owl-next").html('<i class="fas fa-chevron-right"></i>');
+</script>
