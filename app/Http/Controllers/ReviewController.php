c64e0d2d726adc2feb0ee14a564d0fa8bafa77ff
salman432 <salman.ali@tetralogicx.com>
2021-11-26 15:15:54 +0500

    commit

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -470,8 +470,7 @@ class ReviewController extends Controller
 //                $reviews = $reviews->where('review_rating', $request->input('review_stars'))->newQuery();
 //            }
 //        }
-        $reviews = $reviews->get();
-        dd($reviews);
+        $reviews = $reviews->paginate(10);
         return view('pages.review-requests')->with([
             'reviews'=>$reviews,
             'date_range' => $request->input('date-range'),
