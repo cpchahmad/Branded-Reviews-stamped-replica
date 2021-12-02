6e3ee5eb7f94cdfd2037f38afd2f2c602eed2021
salman432 <salman.ali@tetralogicx.com>
2021-12-02 10:23:43 +0500

    commit

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -1,4 +1,6 @@
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200&family=Roboto:ital,wght@0,300;1,900&display=swap" rel="stylesheet">
 </head>
 @extends('layouts.app')
