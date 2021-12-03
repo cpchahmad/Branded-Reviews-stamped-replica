8b22cea5bda189abd5b0e9a1769e50c3916baf56
salman432 <salman.ali@tetralogicx.com>
2021-12-03 17:04:43 +0500

    commit

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -98,13 +98,13 @@ class ReviewController extends Controller
         }
         $review->save();
         if ($review->status == 'publish'){
-            return Redirect::tokenRedirect('review.view', ['id' => $id], ['notice' => 'Review Publish Successfully']);
+            return Redirect::tokenRedirect('review.view', ['id' => $id,'notice' => 'Review Publish Successfully']);
         }
         if ($review->status == 'unpublish'){
-            return Redirect::tokenRedirect('review.view', ['id' => $id], ['notice' => 'Review UnPublish Successfully']);
+            return Redirect::tokenRedirect('review.view', ['id' => $id,'notice' => 'Review UnPublish Successfully']);
         }
         if ($review->status == 'rejected'){
-            return Redirect::tokenRedirect('review.view', ['id' => $id], ['notice' => 'Review Rejected Successfully']);
+            return Redirect::tokenRedirect('review.view', ['id' => $id,'notice' => 'Review Rejected Successfully']);
         }
 //            return \redirect()->back();
     }
