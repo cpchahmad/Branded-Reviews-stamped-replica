cc35e5c54b9e217215b8f504656eec60aece6c43
salman432 <salman.ali@tetralogicx.com>
2021-12-06 15:39:49 +0500

    commit

--- resources/views/append/reviews.blade.php
+++ resources/views/append/reviews.blade.php
@@ -26,6 +26,7 @@
                             </h3>
                         </div>
                     </div>
+                    <img src="{{asset('polished/badge2.png')}}" width="10px" height="10px">
                     <div class="user_info" id="user_info">
                         <h3>{{ucwords($review->name)}}<span>@if($review->verify_status == 'verified') Verified Buyer @endif</span></h3>
                         <p>
