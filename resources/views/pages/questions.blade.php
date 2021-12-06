43230f09ff85809acae028b24f88391d0370fdc9
salman432 <salman.ali@tetralogicx.com>
2021-12-06 13:59:05 +0500

    commit

--- resources/views/pages/questions.blade.php
+++ resources/views/pages/questions.blade.php
@@ -167,7 +167,6 @@
                 border-radius: 27px;
             }
         </style>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <div class="container">
             <div class="row">
                 <div class="profile-header-container">
