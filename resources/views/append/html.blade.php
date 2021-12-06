f1464c08a1ccae7383e89d0b8d7f420d46293a33
salman432 <salman.ali@tetralogicx.com>
2021-12-06 13:03:46 +0500

    commit

--- resources/views/append/html.blade.php
+++ resources/views/append/html.blade.php
@@ -120,7 +120,7 @@
         opacity: .5;
     }
     .review_progress_1:hover{
-        opacity: 1 ;
+        opacity: 1 !important;
     }
     @media (max-width:767px) {
         .review_progress{
