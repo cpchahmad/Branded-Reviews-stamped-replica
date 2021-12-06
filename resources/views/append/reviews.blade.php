a98f1e056af0a1784b96ffc62a7e6e4f565378d0
salman432 <salman.ali@tetralogicx.com>
2021-12-06 18:13:43 +0500

    commit

--- resources/views/append/reviews.blade.php
+++ resources/views/append/reviews.blade.php
@@ -36,7 +36,7 @@
                     <div class="user_info" id="user_info">
                         <h3>{{ucwords($review->name)}}<span>@if($review->verify_status == 'verified') Verified Buyer @endif</span></h3>
                         <p>
-                            <i class="fas fa-flag-usa"></i>
+                            <i class="flag flag {{$review->country_code}}"></i>
                             {{$review->customer_location}}
                         </p>
                         <div>
@@ -197,7 +197,7 @@
                         <div class="user_info" id="user_info">
                             <h3>{{ucwords($review->name)}}<span>@if($review->verify_status == 'veified') Verified Buyer @endif</span></h3>
                             <p>
-                                <i class="fas fa-flag-usa"></i>
+                                <i class="flag flag {{$review->country_code}}"></i>
                                 {{$review->customer_location}}
                             </p>
                             <div>
