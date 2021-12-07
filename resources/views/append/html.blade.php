4ef7d7a151fbff7cd8dcb4a61649ef37835b349c
salman432 <salman.ali@tetralogicx.com>
2021-12-07 15:36:01 +0500

    commit

--- resources/views/append/html.blade.php
+++ resources/views/append/html.blade.php
@@ -2365,11 +2365,12 @@
         <div class="item" id="item">
 
         </div>
-        <div style="position: absolute; top: 0; right: 0;">
-            <button type="button" @if(isset($display)) style="background-color: {{$display->circle_background}}" @endif class="btn-close for-overlay">
-                <span class="icon-cross"></span>
-                <span class="visually-hidden">Close</span>
-            </button>
+        <div style="position: absolute; top: 0; right: 0;margin-right: 1%;">
+{{--            <button type="button" @if(isset($display)) style="background-color: {{$display->circle_background}}" @endif class="btn-close for-overlay">--}}
+{{--                <span class="icon-cross"></span>--}}
+{{--                <span class="visually-hidden">Close</span>--}}
+{{--            </button>--}}
+            <i class="fal fa-times" style="font-size: 43px;"></i>
         </div>
         {{--            <div style="position: absolute;  bottom: 280px; left: 0;">--}}
         {{--                <button type="button" style="color: white; font-size: large;" class="btn-close btn-view">--}}
