9e5cc82ce1926bb934951fe74f43c971f37f5d16
salman432 <salman.ali@tetralogicx.com>
2021-12-07 18:20:58 +0500

    commit

--- resources/views/append/html.blade.php
+++ resources/views/append/html.blade.php
@@ -1248,9 +1248,9 @@
         <div class="tt_logix_reviews_inner">
             <div class="tt_logix_reviews_r_no">
                 <h1>
-                    <span id="total_rating" style="margin: 0px; margin-right: 7px;">{{$total_rating}}</span>
+                    <span id="total_rating" style="margin: 0; margin-right: 5%; margin-top: 1%;">{{$total_rating}}</span>
                          <div class="col-md-6" style="overflow: hidden;">
-                                   <span class="stars-container stars-{{($review_value / 5) * 100}}" style="font-size: xx-large;">★★★★★</span>
+                                   <span class="stars-container stars-{{($review_value / 5) * 100}}" style="font-size: 30px;">★★★★★</span>
                          </div>
                 </h1>
                 <p class="base_reviews base_reviews2">Based on <span id="base_reviews">{{$total_reviews}}</span> Reviews</p>
