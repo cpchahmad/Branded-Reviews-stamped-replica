887a139cf1e8fdb3b645868e20f74bced09c2bfe
salman432 <salman.ali@tetralogicx.com>
2021-12-15 15:32:47 +0500

    commit

--- resources/views/append/html.blade.php
+++ resources/views/append/html.blade.php
@@ -1555,11 +1555,13 @@ color:black;
                         @if(count($review->medias) > 0)
                             @foreach($review->medias as $key => $media)
                                 @if($counter <= 7)
-                                    <a href="javascript:void(0)" title="My Watch" data-value="{{$media->id}}" data-target="myPopup" class="btn-view @if($counter == 3 || $counter == 7) for-image-hidden @endif">
-                                        <div>
-                                            <img src="{{asset('review-images'.'/'.$media->review_media)}}" width="70"  height="55" alt="Picture">
-                                        </div>
-                                    </a>
+                                   <div>
+                                       <a href="javascript:void(0)" title="My Watch" data-value="{{$media->id}}" data-target="myPopup" class="btn-view @if($counter == 3 || $counter == 7) for-image-hidden @endif">
+                                           <div>
+                                               <img src="{{asset('review-images'.'/'.$media->review_media)}}" width="70"  height="55" alt="Picture">
+                                           </div>
+                                       </a>
+                                   </div>
                                     @php $counter += 1; @endphp
                                 @endif
                             @endforeach
