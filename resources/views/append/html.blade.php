b9c3859c94b0c23f6a905614fe5771de7ec4c672
salman432 <salman.ali@tetralogicx.com>
2021-12-24 16:25:25 +0500

    commit

--- resources/views/append/html.blade.php
+++ resources/views/append/html.blade.php
@@ -490,7 +490,7 @@
         }
         .question_review_buttons2{
             margin-top: 3%;
-            margin-left: 6%;
+            margin-left: 5%;
             margin-right: 16px;
         }
     }
@@ -1589,7 +1589,7 @@ color:black;
                                 @if($counter <= 7)
                                        <a href="javascript:void(0)" title="My Watch" data-value="{{$media->id}}" data-target="myPopup" class="btn-view @if($counter == 3 || $counter == 7) for-image-hidden @endif">
                                            <div class="@if($counter == 7) for-image-border @endif">
-                                               <img src="{{asset('review-images'.'/'.$media->review_media)}}" width="70"  height="55" alt="Picture">
+                                               <img src="{{asset('review-images'.'/'.$media->review_media)}}" width="74"  height="55" alt="Picture">
                                            </div>
                                        </a>
                                     @php $counter += 1; @endphp
