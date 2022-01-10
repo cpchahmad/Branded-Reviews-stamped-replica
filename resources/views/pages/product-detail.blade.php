722a9d287a84cb50ecad7ace2ce62abc411dfccb
salman432 <salman.ali@tetralogicx.com>
2022-01-10 11:58:19 +0500

    commit

--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -134,7 +134,7 @@
     </style>
     <div class="col-lg-12 col-md-12 pl-4 pt-3 pr-4">
         <div class="row">
-            <div class="col-6">
+            <div class="col-6 col-sm-12">
                 <h3>Product Reviews Detail</h3>
             </div>
         </div>
